b3a4763bb8ea107109fde2589005b7903f411031
prathmeshkurhade <prathmeshkurhade33@gmail.com>
2025-06-25 11:38:53 +0530

    changed message schema

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./src/lib/mongoose.config.js";
+import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config();    
 const PORT = process.env.PORT;
 const app = express();
+
+app.use("/api/auth", authRoutes);
+
 const startServer = ()=>{
     app.listen(PORT,()=>{
         console.log(`Server started on port ${PORT}`)
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { signup, login } from '../controllers/auth.controller.js';
+import { signup, login, checkauth } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
 
 router.post('/signup',signup)
 router.post('/login', login)
+router.get('/check',protectRoute, checkauth )
+
