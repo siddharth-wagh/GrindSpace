959bff6e68e173927dbebf1dbfbaaa3125ea87b5
sid <siddhuwagh27112004@gmail.com>
2025-06-25 16:20:09 +0530

    added get messages and get groups

--- backend/src/controllers/group.controller.js
+++ backend/src/controllers/group.controller.js
@@ -1,4 +1,6 @@
+
 import Group from "../models/group.model.js";
+import User from "../models/user.model.js"
 export const createGroup = async (req,res)=>{
     try {
         const {name,description,isPublic,tags} = req.body;
@@ -9,7 +11,7 @@ export const createGroup = async (req,res)=>{
         }
         
         //todo: add logic for private room
-
+        
         const group = await Group.create({
             name,
             description,
@@ -23,10 +25,70 @@ export const createGroup = async (req,res)=>{
         if(!group){
             return res.status(400).json({ message:"invalid info"});
         }
-
+        await User.findByIdAndUpdate(leader, {
+                $push: { groups: group._id }
+            });
         return res.status(200).json({
             data:group,
             message:"Group created successfully"
+        });
+    } catch (error) {
+         return res.status(500).json({
+            message:error.message,
+            success:false
+        })
+    }
+}
+export const getAllGroups = async(req,res)=>{
+    try {
+        
+        const userId = req.user._id;
+       
+        const userWithGroups  = await User.findById(userId).populate("groups");
+
+
+          if (!userWithGroups) {
+            return res.status(404).json({ message: "User not found" });
+         }   
+        return res.status(200).json({
+            data:userWithGroups.groups,
+            message:"Group fetched successfully"
+        });
+    } catch (error) {
+         return res.status(500).json({
+            message:error.message,
+            success:false
+        })
+    }
+}
+
+export const joinGroup = async(req,res)=>{
+    try {
+        
+        const userId = req.user._id;
+        const groupId = req.params.groupId;
+        //todo for private
+
+        const group = await Group.findById(groupId);
+
+       
+        if(!group) {
+             return res.status(404).json({ message: "group not found" });
+        } 
+
+        if (!group.members.includes(userId)) {
+                await Group.findByIdAndUpdate(groupId, {
+                $addToSet: { members: userId } // avoids duplicates
+            });
+        }
+
+         await User.findByIdAndUpdate(userId, {
+            $addToSet: { groups: groupId }
+        });
+
+        return res.status(200).json({
+            data:true,
+            message:"Group joined successfully"
         });
     } catch (error) {
          return res.status(500).json({
